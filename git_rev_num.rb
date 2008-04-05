0ce89e874bfcf9a8759b76eb636125f0cef62e20
Scott Taylor <smt@scott-taylors-macbook.local>
2008-04-05 06:40:15 -0400

    * Refactoring

--- git_rev_num.rb
+++ git_rev_num.rb
@@ -2,43 +2,52 @@
 # A one off script for creating global commit
 # numbers for SHA1/git commit hexadecimals
 
-def repository_root
-  "."
+class Repository
+  def branches
+    %x(git-branch).map { |branch| extract_branch_name(branch) }
+  end
+
+  def extract_branch_name(branch_name)
+    branch_name.gsub!("\s.+", "")
+    branch_name.gsub!("\*", "")
+    branch_name.strip
+  end
+  
+  private :extract_branch_name
+  
+  def find_commits
+    %x(git-rev-list #{branches.join(" ")})
+  end
+
+  def commits
+    @commits ||= find_commits.split.reverse
+  end
 end
 
-def branches
-  %x(git-branch).map { |branch| extract_branch_name(branch) }
+class CommitLibrarian
+  attr_reader :commits
+
+  def initialize
+    @repository = Repository.new
+    @commits = @repository.commits
+  end
+
+  def head_rev
+    commits.size + 1
+  end
+  
+  def find_commit_by_sha1(hash)
+    commits.select { |e| e == hash }
+  end
+  
+  def find_commit_by_rev_number(number)
+    commits[number - 1]
+  end
 end
 
-def extract_branch_name(branch_name)
-  branch_name.gsub!("\s.+", "")
-  branch_name.gsub!("\*", "")
-  branch_name.strip
-end
+lookup = CommitLibrarian.new
 
-private :extract_branch_name
-
-def find_commits
-  %x(git-rev-list #{branches.join(" ")})
-end
-
-def commits
-  @commits ||= find_commits.split.reverse
-end
-
-def head_rev
-  commits.size + 1
-end
-
-def find_commit_by_sha1(hash)
-  commits.select { |e| e == hash }
-end
-
-def find_commit_by_rev_number(number)
-  commits[number - 1]
-end
-
-puts find_commits
-puts head_rev
-puts find_commit_by_rev_number(1)
-puts find_commit_by_sha1("df35ddd3d4f7dc13b735d91e068c1f005dcdb8ac")
+puts Repository.new.find_commits
+puts lookup.head_rev
+puts lookup.find_commit_by_rev_number(1)
+puts lookup.find_commit_by_sha1(lookup.find_commit_by_rev_number(1))

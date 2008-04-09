b38052afc47a8eb942a7b11155265a67d928f779
Scott Taylor <smt@scott-taylors-macbook.local>
2008-04-08 22:29:31 -0400

    * Changing Git::GlobalRevisionNumbers to GitRevisionNumbers

--- lib/git_rev_num.rb
+++ lib/git_rev_num.rb
@@ -1,48 +1,47 @@
 # A one off script for mapping
 # git SHA1 hashes to global commit numbers
-module Git
-  module GlobalRevisionNumbers
-    class Repository
-      def branches
-        %x(git-branch).map { |branch| extract_branch_name(branch) }
-      end
-
-      def extract_branch_name(branch_name)
-        branch_name.gsub!("\s.+", "")
-        branch_name.gsub!("\*", "")
-        branch_name.strip
-      end
-
-      private :extract_branch_name
-
-      def find_commits
-        %x(git-rev-list #{branches.join(" ")} --)
-      end
-
-      def commits
-        @commits ||= find_commits.split.reverse
-      end
+module GitRevisionNumbers
+  class Repository
+    def branches
+      %x(git-branch).map { |branch| extract_branch_name(branch) }
     end
 
-    class CommitLibrarian
-      attr_reader :commits
+    def extract_branch_name(branch_name)
+      branch_name.gsub!("\s.+", "")
+      branch_name.gsub!("\*", "")
+      branch_name.strip
+    end
 
-      def initialize
-        @repository = Repository.new
-        @commits = @repository.commits
-      end
+    private :extract_branch_name
 
-      def head_rev
-        commits.size + 1
-      end
+    def find_commits
+      %x(git-rev-list #{branches.join(" ")} --)
+    end
 
-      def find_commit_by_sha1(hash)
-        commits.select { |e| e == hash }
-      end
+    def commits
+      @commits ||= find_commits.split.reverse
+    end
+  end
 
-      def find_commit_by_rev_number(number)
-        commits[number - 1]
-      end
+  class CommitLibrarian
+    attr_reader :commits
+
+    def initialize
+      @repository = Repository.new
+      @commits = @repository.commits
+    end
+
+    def head_rev
+      commits.size + 1
+    end
+
+    def find_commit_by_sha1(hash)
+      commits.select { |e| e == hash }
+    end
+
+    def find_commit_by_rev_number(number)
+      commits[number - 1]
     end
   end
 end
+

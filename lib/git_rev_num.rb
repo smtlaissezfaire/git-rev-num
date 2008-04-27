a86a21001763722b082378c8546c37fddbc0c63c
Scott Taylor <smt@scott-taylors-macbook.local>
2008-04-27 05:57:47 -0400

    * Requiring files

--- lib/git_rev_num.rb
+++ lib/git_rev_num.rb
@@ -1,3 +1,4 @@
-module GitRevisionNumbers
-end
 
+require File.dirname(__FILE__) + "/git_rev_num/repository"
+require File.dirname(__FILE__) + "/git_rev_num/commit_librarian"
+require File.dirname(__FILE__) + "/git_rev_num/master_commit_librarian"

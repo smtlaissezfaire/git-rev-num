74951aec732ad2a6698fd3de01ef83716f810214
Scott Taylor <smt@scott-taylors-macbook.local>
2008-04-27 18:36:59 -0400

    * Adding init directory to MasterCommitLibrarian

--- lib/git_rev_num/master_commit_librarian.rb
+++ lib/git_rev_num/master_commit_librarian.rb
@@ -1,6 +1,6 @@
 module GitRevisionNumbers
   class MasterCommitLibrarian < CommitLibrarian
-    def initialize
+    def initialize(init_directory=".")
       super
       @commits = @repository.commits(:master)
     end

11f056f80dadf8b4deee5963835ed67fa7871fc7
Scott Taylor <smt@scott-taylors-macbook.local>
2008-04-27 05:54:56 -0400

    * Adding CommitLibrarian into it's own file

--- lib/git_rev_num.rb
+++ lib/git_rev_num.rb
@@ -1,29 +1,4 @@
 module GitRevisionNumbers
-  class CommitLibrarian
-    attr_reader :commits
-
-    def initialize
-      @repository = Repository.new
-      @commits = @repository.commits
-    end
-
-    def head_rev
-      commits.size + 1
-    end
-
-    def find_commit_by_sha1(hash)
-      commits.select { |e| e == hash }
-    end
-
-    def find_commit_by_sha1(hash)
-      commits.select { |e| e == hash }
-    end
-
-    def find_commit_by_rev_number(number)
-      commits[number - 1]
-    end
-  end
-
   class MasterCommitLibrarian < CommitLibrarian
     def initialize
       super

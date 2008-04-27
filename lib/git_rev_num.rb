a32321d5c3696aed28419019741b0262b4b45602
Scott Taylor <smt@scott-taylors-macbook.local>
2008-04-27 05:56:39 -0400

    Moving MasterCommitLibrarian into it's own file

--- lib/git_rev_num.rb
+++ lib/git_rev_num.rb
@@ -1,9 +1,3 @@
 module GitRevisionNumbers
-  class MasterCommitLibrarian < CommitLibrarian
-    def initialize
-      super
-      @commits = @repository.commits(:master)
-    end
-  end
 end
 
---
+++ lib/git_rev_num/master_commit_librarian.rb
@@ -0,0 +1,8 @@
+module GitRevisionNumbers
+  class MasterCommitLibrarian < CommitLibrarian
+    def initialize
+      super
+      @commits = @repository.commits(:master)
+    end
+  end
+end

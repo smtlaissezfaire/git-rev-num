dff94ce69f0037b7493502e33d810eaad142f1c0
Scott Taylor <smt@scott-taylors-macbook.local>
2008-04-27 05:56:22 -0400

    * Adding CommitLibrarian

---
+++ lib/git_rev_num/commit_librarian.rb
@@ -0,0 +1,26 @@
+module GitRevisionNumber
+  class CommitLibrarian
+    attr_reader :commits
+
+    def initialize
+      @repository = Repository.new
+      @commits = @repository.commits
+    end
+
+    def head_rev
+      commits.size + 1
+    end
+
+    def find_commit_by_sha1(hash)
+      commits.select { |e| e == hash }
+    end
+
+    def find_commit_by_sha1(hash)
+      commits.select { |e| e == hash }
+    end
+
+    def find_commit_by_rev_number(number)
+      commits[number - 1]
+    end
+  end
+end
